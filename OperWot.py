a058ce1adce9f349099427f0f8d87e4119b3a87b
kjb4494 <yuri_0210@naver.com>
2018-07-10 12:43:40 +0900

    Google Safe Browsing 기반 malware 검사모듈 베이스 구축

--- OperWot.py
+++ OperWot.py
@@ -1,33 +1,27 @@
 import requests
-import json
+import pprint
 
 
 class OperWot:
     def __init__(self):
         self.apiKey = 'AIzaSyBZFkK1_OPvK3zbiwI6z5okk6-yu68b4P0'
-        self.clientId = '381603779315-hcucm7hb0qjhuq1rkckgqvbubijodoaq.apps.googleusercontent.com '
 
-    def rqUrlReport(self):
+    def rqUrlReport(self, sUrl):
         url = "https://safebrowsing.googleapis.com/v4/threatMatches:find"
-        payload = {'client': {'clientId': 'yourcompanyname', 'clientVersion': "1.5.2"},
-                   'threatInfo': {'threatTypes': ["SOCIAL_ENGINEERING", "MALWARE"],
+        payload = {'threatInfo': {'threatTypes': ["MALWARE"],
                                   'platformTypes': ["WINDOWS"],
                                   'threatEntryTypes': ["URL"],
-                                  'threatEntries': [{'url': "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=safsaf"},
-                                                    {"url": "http://www.urltocheck1.org/"},
-                                                    {"url": "http://www.urltocheck2.org/"},
-                                                    {"url": "http://www.urltocheck3.com/"}
+                                  'threatEntries': [{'url': sUrl}
                                                     ]}}
         params = {'key': self.apiKey}
         r = requests.post(url, params=params, json=payload)
-        # Print response
-        print(r)
-        print(r.json())
+        # return response
+        return r.json()
 
 
 def test():
     obOperWot = OperWot()
-    obOperWot.rqUrlReport()
+    pprint.pprint(obOperWot.rqUrlReport("http://malware.testing.google.test/testing/malware/"))
 
 
 if __name__ == '__main__':
